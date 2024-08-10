78b0e9f0f2f873cd9b1040c6e0a6d4c8c1fdf11a
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-08-10 07:44:06 +0700

    todo repository and service

--- src/repositories/todo.repository.ts
+++ src/repositories/todo.repository.ts
@@ -1,9 +1,46 @@
+import { ITodo } from "../entities/interface";
+import { Todo } from "../models/schema";
+
 const TodoRepository = {
-  getAll: async () => {},
-  getOne: async () => {},
-  create: async () => {},
-  update: async () => {},
-  delete: async () => {},
+  getAll: async () => {
+    try {
+      const allTodos = await Todo.find().populate("userId");
+      return allTodos;
+    } catch (error) {
+      console.log(`Repository Error: ${error}`);
+    }
+  },
+  getOne: async (id: string) => {
+    try {
+      const singleTodo = await Todo.findById(id);
+      return singleTodo;
+    } catch (error) {
+      console.log(`Repository Error: ${error}`);
+    }
+  },
+  create: async (todo: ITodo) => {
+    try {
+      const newTodo = new Todo(todo);
+      await newTodo.save();
+    } catch (error) {
+      console.log(`Repository Error: ${error}`);
+    }
+  },
+  update: async (id: string, todo: ITodo) => {
+    try {
+      const updateTodo = await Todo.findByIdAndUpdate(id, todo);
+      return updateTodo;
+    } catch (error) {
+      console.log(`Repository Error: ${error}`);
+    }
+  },
+  delete: async (id: string) => {
+    try {
+      await Todo.findByIdAndDelete(id);
+    } catch (error) {
+      console.log(`Repository Error: ${error}`);
+    }
+  },
 };
 
 export default TodoRepository;
--- src/services/todo.service.ts
+++ src/services/todo.service.ts
@@ -1,9 +1,53 @@
+import { ITodo } from "../entities/interface";
+import TodoRepository from "../repositories/todo.repository";
+
 const TodoServices = {
-  getAll: async () => {},
-  getOne: async () => {},
-  create: async () => {},
-  update: async () => {},
-  delete: async () => {},
+  getAll: async () => {
+    try {
+      const allTodos = await TodoRepository.getAll();
+      return allTodos;
+    } catch (error) {
+      console.log(`Service Error: ${error}`);
+    }
+  },
+  getOne: async (id: string) => {
+    try {
+      const singleTodo = await TodoRepository.getOne(id);
+      return singleTodo;
+    } catch (error) {
+      console.log(`Service Error: ${error}`);
+    }
+  },
+  create: async (newTodo: ITodo) => {
+    try {
+      const { userId, todo } = newTodo;
+      if (!userId || !todo) {
+        throw new Error("userId and todo is required!");
+      }
+      return await TodoRepository.create(newTodo);
+    } catch (error) {
+      console.log(`Service Error: ${error}`);
+      throw error;
+    }
+  },
+  update: async (id: string, todo: ITodo) => {
+    try {
+      if (!todo.todo) {
+        throw new Error("todo is required!");
+      }
+      return await TodoRepository.update(id, todo);
+    } catch (error) {
+      console.log(`Service Error: ${error}`);
+      throw error;
+    }
+  },
+  delete: async (id: string) => {
+    try {
+      return await TodoRepository.delete(id);
+    } catch (error) {
+      console.log(`Service Error: ${error}`);
+    }
+  },
 };
 
 export default TodoServices;

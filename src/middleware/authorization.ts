333eac6014574f98fc01bab1092cf6c26ab59bcd
ZacharyTheodoreCodes <zacharytheodore.zac@gmail.com>
2024-08-11 12:13:41 +0700

    authorization middleware

--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -9,27 +9,22 @@ export const authorization = async (
 ) => {
   const { accessToken, refreshToken } = req.cookies;
 
-  // Screening both tokens
   if (!accessToken || !refreshToken) {
     return res.status(401).json({ message: "Unauthorized, Login required!" });
   }
 
-  // Screening accessToken
   if (accessToken) {
     try {
       jwt.verify(accessToken, process.env.JWT_ACCESS_KEY as string);
     } catch (error) {
-      // Screening refreshToken
       if (!refreshToken) {
         return res.status(401).json({ message: "Unauthorized" });
       }
 
-      // Generate new accessToken
       try {
         jwt.verify(refreshToken, process.env.JWT_REFRESH_KEY as string);
         const validRefTok = await AuthServices.getAuth(refreshToken);
 
-        // Screening refreshToken on Db
         if (!validRefTok) {
           return res.status(401).json({ message: "Unauthorized" });
         }
